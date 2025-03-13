ccbf743cf587f3da86bdcdb0437983eb5b5a7c37
rafrazumba <rafrazumba@gmail.com>
2025-03-13 23:35:20 +0000

    Icono y nombre

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tuempresa.myapp',
-  appName: 'myapp',
+  appName: 'Love Tester',
   webDir: 'dist/myapp/browser',
 };
 
