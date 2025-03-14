154c340e6ed6f312476be01f2ac34c463febfa16
rafrazumba <rafrazumba@gmail.com>
2025-03-14 00:02:55 +0000

    css arreglao

--- src/app/compatibility-game/compatibility-game.component.ts
+++ src/app/compatibility-game/compatibility-game.component.ts
@@ -1,3 +1,5 @@
+//compatibility-game.component.ts
+
 import { Component, type OnInit } from "@angular/core"
 import { FormsModule } from "@angular/forms"
 import { CommonModule } from "@angular/common"
